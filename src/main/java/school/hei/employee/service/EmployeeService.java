7c4a705aab89bc42c7cd359256d492579f7d9920
Andraina <hei.andraina.4@gmail.com>
2026-05-31 20:55:14 +0300

    feat: add EmployeeService and EmployeeController

---
+++ src/main/java/school/hei/employee/service/EmployeeService.java
@@ -0,0 +1,38 @@
+package school.hei.employee.service;
+
+import org.springframework.stereotype.Service;
+import school.hei.employee.entity.Employee;
+import school.hei.employee.repository.EmployeeRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Employee> findAll(String department, Boolean actif) throws SQLException {
+        return repository.findAll(department, actif);
+    }
+
+    public Employee findById(Long id) throws SQLException {
+        return repository.findById(id);
+    }
+
+    public Employee save(Employee employee) throws SQLException {
+        return repository.save(employee);
+    }
+
+    public Employee update(Long id, Employee employee) throws SQLException {
+        return repository.update(id, employee);
+    }
+
+    public void delete(Long id) throws SQLException {
+        repository.delete(id);
+    }
+
+}
